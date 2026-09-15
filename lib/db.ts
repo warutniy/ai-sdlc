97911c13f579fe057679218b6920135eed061a00
warut niyomka <warut.n@mapboss.co.th>
2026-09-15 23:52:30 +0700

    fix: update data 1

--- lib/db.ts
+++ lib/db.ts
@@ -351,7 +351,7 @@ function seed(db: DatabaseSync) {
         emoji: "🥤",
         category: "coffees-teas",
         section: null,
-        rating: 4,
+        rating: 5,
         reviewCount: 5,
         price: "$18.40",
         oldPrice: "$26.0",
