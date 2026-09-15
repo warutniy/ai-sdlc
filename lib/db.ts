6b0d7fa6ca2860f07d4b2abed533688bc35d154f
warut niyomka <warut.n@mapboss.co.th>
2026-09-16 01:02:46 +0700

    fix: update data reset 1

--- lib/db.ts
+++ lib/db.ts
@@ -320,7 +320,7 @@ function seed(db: DatabaseSync) {
         emoji: "🍯",
         category: "breads-sweets",
         section: "just-landing",
-        rating: 5,
+        rating: 6,
         reviewCount: 11,
         price: "$9.4",
         oldPrice: null,
